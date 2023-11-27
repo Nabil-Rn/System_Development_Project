1c7814239dc9d8110b445dc8954c0fdad5e7f451
Peter Fishman <98246441+PeterFishmann@users.noreply.github.com>
2023-11-26 22:32:04 -0500

    Fixed inversed files

--- MVC/Views/Admin/view_appointments.php
+++ MVC/Views/Admin/view_appointments.php
@@ -14,33 +14,6 @@
 <body>
     <?php include_once "../../navbar.php"; ?>
 
-    <div class="admin-container">
-        <h1>Modify Availability</h1>
-        <div id="availability-setting">
-            <!-- Google Calendar API for setting availabilities -->
-        </div>
-        <div id="availability-calendar">
-            <!-- Google Calendar API display the calendar -->
-        </div>
-    </div>
-
-
-    <!-- Google Calendar API Library (Searched online from Google to attempt to set this up) -->
-    <script src="https://apis.google.com/js/api.js"></script>
-
-    <script>
-        // Load the Google APIs Client Library and initialize the Calendar API
-        gapi.load('client', function() {
-            gapi.client.init({
-                apiKey: 'YOUR_API_KEY',
-                discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-            }).then(function() {
-                // Google Calendar API is loaded and ready to be used
-                // You can call gapi.client.calendar.[API_METHODS] here
-            });
-        });
-    </script>
-
     <footer>
         <?php include '../../footer.php'; ?>
     </footer>
