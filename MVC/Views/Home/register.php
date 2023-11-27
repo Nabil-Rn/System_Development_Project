d22df701f73f51efb0c0e3af0be8186bc7700940
Peter Fishman <98246441+PeterFishmann@users.noreply.github.com>
2023-11-27 08:45:26 -0500

    Added blanket for signup form in register.php

--- MVC/Views/Home/register.php
+++ MVC/Views/Home/register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,81 +10,84 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
 </head>
+
 <body>
-<header>
-    <div class="header-container">
-        <div class="right-box">
-            <img class="logo-image" src="../../assets/logo.png" alt="Logo" />
-            <div class="title-container"> JustBFitness</div>
+    <header>
+        <div class="header-container">
+            <div class="right-box">
+                <img class="logo-image" src="../../assets/logo.png" alt="Logo" />
+                <div class="title-container"> JustBFitness</div>
+            </div>
+            <div class="right-box">
+                <label><a href="javascript:window.history.back();">LOGIN</a></label>
+            </div>
         </div>
-        <div class="right-box">
-            <label><a href="javascript:window.history.back();">LOGIN</a></label>
-        </div>
-    </div>
-</header>
+    </header>
 
-<div class="title">
+    <div class="title">
         <div class="title-text">Sign Up</div>
-</div>
+    </div>
 
-<div class="all-container">
-    
-        
+    <div class="all-container">
+
+
 
         <div class="all-form">
-            <div class="title-large">Create Your Account: </div>
-               <div class="form-group">        
-                <label><span class="required">*</span>First Name:</label>
-                <div class="input-box"><input type="text" id="fname" required></div>
-        
-         
-                <label><span class="required">*</span>Last Name:</label>
-                <div class="input-box"><input type="text" id="lname" required></div>
-          
- 
-                <label>Phone Number:</label>
-                <div class="input-box"><input type="phone" id="phone"></div>
-          
-      
-                <label><span class="required">*</span>Email:</label>
-                <div class="input-box"><input type="email" id="email" required></div>
-            
-            
-                <label><span class="required">*</span>Password:</label>
-                <div class="input-box"><input type="password" id="password" required></div>
-            
-    
-                <label><span class="required">*</span>Re-Enter Password:</label>
-                <div class="input-box"><input type="password" id="re-password" required></div>
-     
-                <div class="button-container">
-                <div class="button">Sign Up</div>
+            <form action="RegisterProcess.php" method="post">
+                <div class="title-large">Create Your Account: </div>
+                <div class="form-group">
+                    <label><span class="required">*</span>First Name:</label>
+                    <div class="input-box"><input type="text" id="fname" required></div>
 
+
+                    <label><span class="required">*</span>Last Name:</label>
+                    <div class="input-box"><input type="text" id="lname" required></div>
+
+
+                    <label>Phone Number:</label>
+                    <div class="input-box"><input type="phone" id="phone"></div>
+
+
+                    <label><span class="required">*</span>Email:</label>
+                    <div class="input-box"><input type="email" id="email" required></div>
+
+
+                    <label><span class="required">*</span>Password:</label>
+                    <div class="input-box"><input type="password" id="password" required></div>
+
+
+                    <label><span class="required">*</span>Re-Enter Password:</label>
+                    <div class="input-box"><input type="password" id="re-password" required></div>
+
+                    <div class="button-container">
+                    <input type="submit" class="button" value="Sign Up">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <footer>
+        <div class="left-box">
+            <div class="contact-item">
+                <div class="contact-label">Contact Us | Contactez nous</div>
+                <div class="contact-info">
+                    <div class="contact-text"><a href="mailto:enquiries@justbfitness.ca">enquiries@justbfitness.ca</a></div>
+                    <div class="contact-text"><a href="tel:+15148628093">(514) 862-8093</a></div>
+                </div>
+                <div class="social-icons">
+                    <a href="https://facebook.com/JustBfitnessOfficial/"><img src="../../assets/facebook.png"></a>
+                    <a href="https://www.instagram.com/JustBfitness.ca/"><img src="../../assets/instagram.png"></a>
                 </div>
             </div>
         </div>
-    
-</div>
-<footer>
-    <div class="left-box">
-        <div class="contact-item">
-            <div class="contact-label">Contact Us | Contactez nous</div>
-            <div class="contact-info">
-                <div class="contact-text"><a href="mailto:enquiries@justbfitness.ca">enquiries@justbfitness.ca</a></div>
-                <div class="contact-text"><a href="tel:+15148628093">(514) 862-8093</a></div>
-            </div>
-            <div class="social-icons">
-                <a href="https://facebook.com/JustBfitnessOfficial/"><img src="../../assets/facebook.png" ></a>
-                <a href="https://www.instagram.com/JustBfitness.ca/"><img src="../../assets/instagram.png"></a>
+        <div class="right-box">
+            <div class="contact-item">
+                <img class="logo-image" src="../../assets/logo.png" alt="Logo">
+                <div class="copy-rights-text"> &copy; JUST B FITNESS 2023. ALL RIGHTS RESERVED.</div>
             </div>
         </div>
-    </div>
-    <div class="right-box">
-        <div class="contact-item">
-            <img class="logo-image" src="../../assets/logo.png" alt="Logo">
-            <div class="copy-rights-text"> &copy; JUST B FITNESS 2023. ALL RIGHTS RESERVED.</div>
-        </div>
-    </div>
-</footer>
+
+    </footer>
 </body>
+
 </html>
