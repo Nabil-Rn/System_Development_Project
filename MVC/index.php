9eed26ee4eec4f57b4a2565a643470c12698610d
Nabil-Rn <98413966+Nabil-Rn@users.noreply.github.com>
2023-12-11 11:02:54 -0500

    index?
    fixed the index?

--- MVC/index.php
+++ MVC/index.php
@@ -1,61 +1,16 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 
-
-// Start the session
-// Uncomment the line below once you've confirmed where the issue lies
-// session_start();
-
-
-
-require_once 'mysqldatabase.php';
-require_once 'Models/User.php';
-require_once 'Controllers/UserController.php';
-
-
-
-// Instantiate the main controller
-$userController = new UserController();
-
-
-
-// Handle login and logout directly
-if (isset($_POST['action']) && $_POST['action'] === 'login') {
-    echo "<p>Handling login...</p>";
-    $userController->handleLogin();
-}
-
-if (isset($_GET['logout'])) {
-    echo "<p>Handling logout...</p>";
-    $userController->logout();
-    exit;
-}
-
-
-
-// Dynamic controller routing
-$controller = (isset($_GET['controller'])) ? $_GET['controller'] : "Home";
+$controller = (isset($_GET['controller'])) ? $_GET['controller'] : "home";
 $action = (isset($_GET['action'])) ? $_GET['action'] : "index";
 $id = (isset($_GET['id'])) ? intval($_GET['id']) : -1;
 
 $controllerClassName = ucfirst($controller) . "Controller";
-$controllerFilePath = "Controllers/$controllerClassName.php";
+include_once "Controllers/$controllerClassName.php";
 
-if (file_exists($controllerFilePath)) {
-    include_once $controllerFilePath;
 
-    $ct = new $controllerClassName();
-    if (method_exists($ct, $action)) {
-        $ct->$action($id);
-    } else {
-        echo "<p>Action method $action does not exist in $controllerClassName...</p>";
-        // Handle the case where the action method doesn't exist
-    }
-} else {
-    echo "<p>Controller file $controllerFilePath does not exist...</p>";
-    // Handle the case where the controller file doesn't exist
-}
+$ct = new $controllerClassName();
+$ct->route();
+
+
 ?>
